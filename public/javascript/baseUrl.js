027ae5bfe6650c0ffabbb0397753975a0d58ecb9
Chastellano <charles.a.castellano@gmail.com>
2021-05-10 05:57:29 -0400

    gitignore

--- public/javascript/baseUrl.js
+++ public/javascript/baseUrl.js
@@ -1,5 +1,3 @@
-// const baseUrl = 'https://arcane-bayou-09246.herokuapp.com/'
-// const baseUrl = 'localhost:3443/'
-const baseUrl = 'http://localhost:3000/'
+const baseUrl = 'https://arcane-bayou-09246.herokuapp.com/'
 
 export default baseUrl;
